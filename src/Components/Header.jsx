e4414c573ccb23225cf3a85b2f32044f3205d5ba
Ada Okwuchi <okwuchiadanma@gmail.com>
2021-08-10 14:20:03 +0100

    Added mobile responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import '../Styles/header.css'
 import logo from '../Images/logo.png'
 
 function Header() {
 
+    const [click, setClick] = useState(false);
+   
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  
+    
 
     return (
         <div className="header">
             <div className="logo">
             
-                <img src={logo} alt="My name is Ada" />
+                <img src={logo} alt="logo of the name Ada" />
             </div>
-            <nav>
-                <ul className="nav-links">
+
+            <nav className="nav-bar"> 
+            <div className="nav-icon" onClick={handleClick}>
+                <i className={ click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+                <ul className={ click ? 'nav-links active' : 'nav-links'}>
 
                     <li>
-                        <Link to="/" className="nav-link"> Home </Link>
+                        <Link to="/" className="nav-link" onClick={closeMobileMenu}> Home </Link>
                         
                     </li>
                     <li>
-                        <Link to="/about" className="nav-link"> About </Link>
+                        <Link to="/about" className="nav-link" onClick={closeMobileMenu}> About </Link>
                         
                     </li>
                     <li>
-                        <Link  to="/portfolio" className="nav-link"> Portfolio </Link>
+                        <Link  to="/portfolio" className="nav-link" onClick={closeMobileMenu}> Portfolio </Link>
                        
                     </li>
                     <li>
-                        <Link to="/contact" className="nav-link"> Contact </Link>
+                        <Link to="/contact"  className="nav-link" onClick={closeMobileMenu}> Contact </Link>
                        
                     </li>
                 </ul>
